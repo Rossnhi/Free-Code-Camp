6f1bad061323cadd6ffb280529b9ba268ddb0fd0
Rossnhi <52858656+Rossnhi@users.noreply.github.com>
2021-06-25 14:20:38 +0530

    Update Cash-Register.js

--- JS/Cash-Register.js
+++ JS/Cash-Register.js
@@ -1,41 +1,42 @@
 function checkCashRegister(price, cash, cid) {
   let changeNeeded = cash - price;
+  console.log(changeNeeded);
   cid = cid.reverse();
-  let currency = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
   var changeObj = {
-    status: "",
+    status : "",
     change: []
   };
-  for (let i = 0; i < currency.length; i++) {
-    if (changeNeeded/currency[i] >= 1 && cid[i][1] > 0) {
-      let temp = (Math.round(changeNeeded * 100) % Math.round(currency[i] * 100))/100;
-      let chng = (Math.round(changeNeeded * 100) - Math.round(temp * 100))/100;
-      if (chng <= cid[i][1]) {
+  let chng;
+  let denomination = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+  for ( let i = 0; i < cid.length; i++) {
+    if ( changeNeeded/ denomination[i] >= 1) {
+      if ( changeNeeded < cid[i][1]) {
+        chng = parseInt(changeNeeded/denomination[i]) * denomination[i];
+        changeObj.change.push([cid[i][0],chng]);
         changeNeeded = (Math.round(changeNeeded * 100) - Math.round(chng * 100))/100;
       }
       else {
-        chng = cid[i][1];
-        changeNeeded = (Math.round(changeNeeded * 100) - Math.round(chng * 100))/100;
+        changeObj.change.push([cid[i][0], cid[i][1]]);
+        changeNeeded = (Math.round(changeNeeded * 100) - Math.round(cid[i][1] * 100))/100;
       }
-      changeObj.change.push([cid[i][0], chng]);
     }
     else {
-      changeObj.change.push([cid[i][0], 0]);
-    }
-    
-  }
-  if ( changeNeeded == 0) {
-    if (JSON.stringify(changeObj.change)==JSON.stringify(cid)) {
-      changeObj.change.reverse();
-      changeObj.status = "CLOSED";
-    }
-    else {
-      changeObj.change = changeObj.change.filter(ele => ele[1] != 0);
-      changeObj.status = "OPEN";
+        changeObj.change.push([cid[i][0], 0]);
     }
   }
-  else {
-    changeObj.status = "INSUFFICIENT_FUNDS";
-    changeObj.change = [];
-  }
+ if (changeNeeded == 0) {
+   if (JSON.stringify(changeObj.change) == JSON.stringify(cid)) {
+     changeObj.status = "CLOSED";
+     changeObj.change = changeObj.change.reverse();
+   }
+   else {
+     changeObj.status = "OPEN";
+     changeObj.change = changeObj.change.filter(ele => ele[1] != 0);
+   }
+ }
+ else {
+   changeObj.status = "INSUFFICIENT_FUNDS";
+   changeObj.change = [];
+ }
   return changeObj;
+}
